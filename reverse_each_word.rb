97c2c272961baf54505f23b15340541089467c09
Scott Hoisington <hoyzington@gmail.com>
2019-12-11 23:33:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,6 @@
-require "pry"
 
 def reverse_each_word(sentence)
   pieces = sentence.split
   rev_pieces = pieces.collect{|word| word.reverse}
-#binding.pry  
   rev_pieces.join(" ")
-#binding.pry
 end
-
-reverse_each_word("Hello there, and how are you?")
