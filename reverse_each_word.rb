d94f9590d2d0158c31ce48064c6dc37331953807
Scott Hoisington <hoyzington@gmail.com>
2019-12-11 23:09:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,12 @@
+require "pry"
+
 def reverse_each_word(sentence)
   pieces = sentence.split
+binding.pry  
   pieces.collect{|word| word.reverse}
+binding.pry  
   pieces.join(" ")
+binding.pry
 end
+
+reverse_each_word()
